ed1d5775b60c6dc9652ba4facc1fda39e47ea2a2
Fredoski26 <fredrickobarafor@yahoo.com>
2023-03-10 12:44:21 +0100

    dimensions

--- lib/utils/dynamic.dart
+++ lib/utils/dynamic.dart
@@ -8,8 +8,28 @@ class Dimensions {
   static double screenHeight = Get.context!.height;
   static double screenWidth = Get.context!.width;
 
-
+  //height size
   static double height10 = screenHeight / 84.4;
   static double height80 = screenHeight / 10.5;
+  static double height15 = screenHeight / 56.27;
+  static double height20 = screenHeight / 42.2;
+  static double height30 = screenHeight / 28.13;
+  static double height45 = screenHeight / 18.76;
+
+  //radius size
+  static double radius15 = screenHeight / 56.27;
+  static double radius20 = screenHeight / 42.2;
+
+  //width size
+  static double width10 = screenHeight / 84.4;
+  static double width15 = screenHeight / 56.27;
+  static double width20 = screenHeight / 42.2;
+
+  //font size
+  static double font20 = screenHeight / 42.2;
+  static double font16 = screenHeight / 52.75;
+  static double font13 = screenHeight / 64.9;
+  static double font14 = screenHeight / 60.28;
+
 
 }
